6fbe05327d74dee332d7190ee509dc0734bacaae
Sukesh Shetty <thesukeshshetty@gmail.com>
2026-02-09 20:28:35 +0530

    RD ME Upg

--- RAG_env/Scripts/activate.bat
+++ RAG_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Sukesh\Cursor\RAG_ent\RAG_env
+set VIRTUAL_ENV=D:\Sukesh\Anti-Gravity\RAG_ent\RAG_env
 
 if not defined PROMPT set PROMPT=$P$G
 
